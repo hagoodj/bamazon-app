2e8f513d54acbacc95a05f7570ffff1ca0988a46
jordan hagood <jordan.hagood@yahoo.com>
2019-10-02 10:00:58 -0700

    afterConnection function displays all product options

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,20 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+});
+
+function afterConnection() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("Product ID | Product Name | Department | Price | Stock")
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity)
+        }
+        product();
+    });
+}
+
